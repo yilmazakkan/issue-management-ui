c87241e400aa8ec90bfbd424a576738f3a9a5fa8
Yilmaz Akkan <yilmaz.akkan@gmail.com>
2019-08-15 14:57:27 +0300

    template eklendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './_layout/footer/footer.component';
+import { HeaderComponent } from './_layout/header/header.component';
+import { SidebarComponent } from './_layout/sidebar/sidebar.component';
+import { AppLayoutComponent } from './_layout/app-layout/app-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    AppLayoutComponent
   ],
   imports: [
     BrowserModule,
